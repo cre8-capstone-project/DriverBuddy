b4d6e6f82c67f18a05ab1a1a9bc6d42899051477
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2025-01-30 23:03:13 -0800

    fix: CRE8-49 Add worklets-core to optimize animations and camera processing.

---
+++ frontend/babel.config.js
@@ -0,0 +1,9 @@
+// https://docs.expo.dev/router/installation/#modify-babelconfigjs
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [['react-native-reanimated/plugin'], ['react-native-worklets-core/plugin']],
+  };
+};

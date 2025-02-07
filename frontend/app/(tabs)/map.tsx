d94796c87705559a687ca938dfa6dcd180f70c80
cocoysg <cocoy.suguitan@yahoo.com>
2025-02-07 15:13:23 -0800

    feat: Adds Google Maps API and dependencies CRE8-52

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,9 +1,16 @@
 import {View, Text, StyleSheet} from 'react-native';
+import {Button, Card} from '@rneui/themed';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Map Demo</Text>
+      <Button title="React Native Elements Button" />
+      <Card>
+        <Card.Title>React Native Elements Card</Card.Title>
+        <Card.Divider />
+        <Text>Card content goes here.</Text>
+      </Card>
     </View>
   );
 }

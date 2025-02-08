2dc97bb33e4615a854cd6c665136435f83d0b7f6
cocoysg <cocoy.suguitan@yahoo.com>
2025-02-07 16:20:10 -0800

    feat: Adds mapview, react native maps & updated map page #CRE8-89

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,28 +1,53 @@
-import {View, Text, StyleSheet} from 'react-native';
-import {Button, Card} from '@rneui/themed';
+import React, { useRef } from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Button } from 'react-native';
 
-export default function HomeScreen() {
+export default function App() {
+  const mapRef = useRef<MapView>(null);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Map Demo</Text>
-      <Button title="React Native Elements Button" />
-      <Card>
-        <Card.Title>React Native Elements Card</Card.Title>
-        <Card.Divider />
-        <Text>Card content goes here.</Text>
-      </Card>
+    <View style={mapStyles.container}>
+      <MapView
+        ref={mapRef}
+        style={mapStyles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 49.2827, // Default to Vancouver
+          longitude: -123.1207,
+          latitudeDelta: 2, // Smaller = zoomed in
+          longitudeDelta: 2,
+        }}
+      />
+      <View style={mapStyles.buttonContainer}>
+        <Button
+          title="Button Test"
+          onPress={() => {
+            console.log('Button pressed');
+          }}
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+// Styles for map page
+const mapStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  map: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
+    zIndex: 1,
   },
 });

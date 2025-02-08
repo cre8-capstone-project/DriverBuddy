9352065e74f08e02c5e4f60e54accf54e652e60e
cocoysg <cocoy.suguitan@yahoo.com>
2025-02-07 16:59:29 -0800

    feat: Adds user location permissions and displays on map #CRE8-90

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,9 +1,47 @@
-import React, { useRef } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet, View, Button } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, View, Alert} from 'react-native';
+import {Button} from '@rneui/themed';
+
+import * as Location from 'expo-location';
+
+// Define the Region type
+type Region = {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+};
 
 export default function App() {
+  const [location, setLocation] = useState<Region | null>(null);
   const mapRef = useRef<MapView>(null);
+
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert(
+          'Permission Denied',
+          'Please allow location access to show your position on the map.',
+        );
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      const newRegion = {
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      };
+
+      setLocation(newRegion);
+
+      // Zoom to user's location
+      mapRef.current?.animateToRegion(newRegion, 1000); // 1000ms = 1s animation
+    })();
+  }, []);
 
   return (
     <View style={mapStyles.container}>
@@ -11,18 +49,23 @@ export default function App() {
         ref={mapRef}
         style={mapStyles.map}
         provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 49.2827, // Default to Vancouver
-          longitude: -123.1207,
-          latitudeDelta: 2, // Smaller = zoomed in
-          longitudeDelta: 2,
-        }}
+        initialRegion={
+          location || {
+            latitude: 49.2827, // Default to Vancouver
+            longitude: -123.1207,
+            latitudeDelta: 2, // Smaller = zoomed in
+            longitudeDelta: 2,
+          }
+        }
+        showsUserLocation
+        showsMyLocationButton
       />
       <View style={mapStyles.buttonContainer}>
         <Button
           title="Button Test"
           onPress={() => {
             console.log('Button pressed');
+            // Add functionality for the button here
           }}
         />
       </View>

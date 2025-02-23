e18169412f628ede95acda55197b8f36013eba14
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2025-02-22 23:27:03 -0800

    feat: Implement PIP like UI for face detection.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,114 +1,200 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, StyleSheet, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text, Dimensions} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {MaterialIcons} from '@expo/vector-icons';
-import type {StackNavigationProp} from '@react-navigation/stack';
 import {CameraView} from '@/features/safety-alert/components/CameraView';
 import {Map} from '@/app/map';
-import {Button} from '@rneui/themed';
+import {Button, Icon} from '@rneui/themed';
+import type {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 type RootStackParamList = {
   settings: undefined;
   profile: undefined;
 };
 
-type DisplayMode = 'camera' | 'map';
+type ViewMode = 'cameraView' | 'mapView';
+
+const {width, height} = Dimensions.get('window');
 
 export default function HomeScreen() {
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const [displayMode, setDisplayMode] = useState<DisplayMode>('camera');
-  const [isCameraActive, setIsCameraActive] = useState(false);
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const [viewMode, setViewMode] = useState<ViewMode>('cameraView');
+  const [isFaceDetectionActive, setIsFaceDetectionActive] = useState(false);
+  const [cameraKey, setCameraKey] = useState(0);
+
+  useEffect(() => {
+    setCameraKey(prevKey => prevKey + 1);
+  }, [viewMode]);
 
   return (
     <View style={styles.container}>
-      {displayMode === 'camera' ? (
-        <CameraView isCameraActive={isCameraActive} setIsCameraActive={setIsCameraActive} />
-      ) : (
+      {/* Switch View Mode: Camera or Map */}
+      <View
+        style={[
+          styles.componentContainer,
+          // viewMode === 'cameraView' ? styles.visible : styles.hidden,
+          viewMode === 'cameraView'
+            ? styles.visible
+            : isFaceDetectionActive
+              ? styles.cameraPIP
+              : styles.hidden,
+        ]}>
+        <CameraView
+          key={cameraKey}
+          isCameraActive={isFaceDetectionActive}
+          setIsCameraActive={setIsFaceDetectionActive}
+        />
+      </View>
+      <View
+        style={[
+          styles.componentContainer,
+          viewMode === 'mapView' ? styles.visible : styles.hidden,
+        ]}>
         <Map />
-      )}
-      <Button
-        title={displayMode === 'camera' ? 'Turn on Map' : ''}
-        type="outline"
-        icon={
-          displayMode === 'camera' ? (
-            <MaterialIcons name="location-on" size={30} color="black" />
-          ) : (
-            <View style={styles.backButtonContainer}>
-              <MaterialIcons name="west" size={30} color="black" />
-              <Text style={styles.backText}>Back</Text>
-            </View>
-          )
-        }
-        titleStyle={styles.buttonText}
-        containerStyle={[styles.turnOnMapButton, displayMode === 'map' && styles.backButtonStyle]}
-        onPress={
-          displayMode === 'camera' ? () => setDisplayMode('map') : () => setDisplayMode('camera')
-        }
-      />
+      </View>
 
-      {displayMode === 'camera' && (
-        <>
-          <TouchableOpacity
-            style={styles.settingButton}
+      <View style={styles.navContainer}>
+        {/* Back Button */}
+        {viewMode === 'mapView' && (
+          <Button
+            type="clear"
+            containerStyle={styles.backButtonContainer}
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            iconPosition="top"
+            icon={{name: 'west', size: 30, color: 'black'}}
+            onPress={() => setViewMode('cameraView')}>
+            <Text style={styles.buttonText}>Back</Text>
+          </Button>
+        )}
+
+        {/* Setting Button */}
+        {viewMode === 'cameraView' && !isFaceDetectionActive && (
+          <Button
+            type="clear"
+            containerStyle={styles.navButtonContainer}
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            iconPosition="top"
+            icon={{name: 'settings', size: 30, color: 'black'}}
             onPress={() => navigation.navigate('settings')}>
-            <MaterialIcons name="settings" size={30} color="black" />
-            <Text>Setting</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.profileButton}
+            <Text style={styles.buttonText}>Settings</Text>
+          </Button>
+        )}
+
+        {/* Turn Off Face Detection Button */}
+        {isFaceDetectionActive && (
+          <Button
+            type="clear"
+            containerStyle={styles.buttonContainer}
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            onPress={() => setIsFaceDetectionActive(false)}>
+            <Icon name="videocam-off" size={30} color="black" />
+            <Text style={styles.buttonText}>Turn off{'\n'}detection</Text>
+          </Button>
+        )}
+
+        {/* Start Face Detection Button */}
+        {viewMode === 'mapView' && !isFaceDetectionActive && (
+          <Button
+            type="clear"
+            containerStyle={styles.buttonContainer}
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            onPress={() => setIsFaceDetectionActive(true)}>
+            <Icon name="videocam" size={30} color="black" />
+            <Text style={styles.buttonText}>Start{'\n'}detection</Text>
+          </Button>
+        )}
+
+        {/* Map View Button */}
+        {viewMode === 'cameraView' && (
+          <Button
+            type="outline"
+            containerStyle={styles.buttonContainer}
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            onPress={() => setViewMode('mapView')}>
+            <Icon name="location-on" size={30} color="black" />
+            <Text style={styles.buttonText}>Map View</Text>
+          </Button>
+        )}
+
+        {/* Profile Button */}
+        {viewMode === 'cameraView' && !isFaceDetectionActive && (
+          <Button
+            type="clear"
+            containerStyle={styles.navButtonContainer}
+            buttonStyle={styles.button}
+            titleStyle={styles.buttonText}
+            iconPosition="top"
+            icon={{name: 'person', size: 30, color: 'black'}}
             onPress={() => navigation.navigate('profile')}>
-            <MaterialIcons name="person" size={30} color="black" />
-            <Text>Profile</Text>
-          </TouchableOpacity>
-        </>
-      )}
+            <Text style={styles.buttonText}>Profile</Text>
+          </Button>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  turnOnMapButton: {
+  container: {flex: 1},
+  componentContainer: {flex: 1},
+  visible: {display: 'flex'},
+  hidden: {display: 'none'},
+  navContainer: {
     position: 'absolute',
-    bottom: 25,
-    alignSelf: 'center',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     paddingVertical: 10,
-    paddingHorizontal: 5,
-    borderWidth: 2,
-    borderRadius: 40,
-    tintColor: 'black',
   },
-  backButtonStyle: {
-    justifyContent: 'center',
-    // alignItems: 'center',
-    width: 70,
-    height: 70,
-    left: 35,
-    borderRadius: 90,
+  navButtonContainer: {
+    flexDirection: 'column',
+    borderRadius: 30,
+  },
+  buttonContainer: {
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderRadius: 30,
+    tintColor: 'black',
+    width: '45%',
   },
   backButtonContainer: {
-    alignItems: 'center',
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderRadius: 30,
+    tintColor: 'black',
   },
-  backText: {
-    fontSize: 14,
-    color: 'black',
+  button: {
+    borderColor: 'transparent',
+    padding: 0,
+    margin: 0,
+    gap: 5,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    height: 60,
   },
   buttonText: {
+    textAlign: 'center',
     color: 'black',
-    fontWeight: 'bold',
   },
-  settingButton: {
+
+  // TODO: NEED MORE INVESTIGATION
+  cameraPIP: {
+    overflow: 'hidden',
+    // borderColor: 'lightgreen',
+    // borderWidth: 20,
     position: 'absolute',
-    bottom: 30,
-    left: 40,
-    alignItems: 'center',
-  },
-  profileButton: {
-    position: 'absolute',
-    bottom: 30,
-    right: 40,
-    alignItems: 'center',
+    borderRadius: 60,
+    top: -140,
+    left: -125,
+    width: width * 1,
+    height: height * 1,
+    transform: [{scale: 0.2}],
+    zIndex: 1,
   },
 });

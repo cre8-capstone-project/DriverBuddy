9fd2615f12a09fc544a473042249eebe1c452012
v1n1c1u <vinicius.abner1039@gmail.com>
2025-02-26 13:03:08 -0800

    feat: Adds service for getting stops, adding stops, and emptying the stops table

---
+++ frontend/services/StopsService.ts
@@ -0,0 +1,31 @@
+import {drizzleDb} from '@/db/db';
+import {stops} from '@/db/schema';
+import {StopsType} from '@/types/StopsType';
+
+export const StopsTypeService = {
+  async getAllStops() {
+    try {
+      const result = await drizzleDb.select().from(stops);
+      return result;
+    } catch (error) {
+      console.error('Retrieve Stop data failed:', error);
+      throw error;
+    }
+  },
+  async addStop(stopObj: StopsType) {
+    try {
+      await drizzleDb.insert(stops).values(stopObj);
+    } catch (error) {
+      console.error('Add Stop data failed:', error);
+      throw error;
+    }
+  },
+  async emptyStopTable() {
+    try {
+      await drizzleDb.delete(stops);
+    } catch (error) {
+      console.error('Emptying Stop table failed:', error);
+      throw error;
+    }
+  },
+};
---
+++ frontend/types/StopsType.ts
@@ -0,0 +1,12 @@
+export type StopsType = {
+  stopId: number;
+  journeyId: string;
+  latitude: number;
+  longitude: number;
+  address: string;
+  name: string;
+  priceLevel: number;
+  rating: number;
+  isOrigin: boolean;
+  isDestination: boolean;
+};

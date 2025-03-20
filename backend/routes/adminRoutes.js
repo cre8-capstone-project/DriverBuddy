982277f93f4bb0b14026ddade9233aeb386c3d63
v1n1c1u <vinicius.abner1039@gmail.com>
2025-03-20 09:01:15 -0700

    fix: Adds correct extension to javascript file import in the backend

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import express from 'express';
-import authenticateToken from '../authenticateToken';
+// eslint-disable-next-line import/extensions
+import authenticateToken from '../authenticateToken.js';
 
 const adminsRoutes = adminsCollection => {
   const router = express.Router();

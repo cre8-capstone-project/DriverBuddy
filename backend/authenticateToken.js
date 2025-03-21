b756020066e7e9b86070dbed7f95231c55082484
v1n1c1u <vinicius.abner1039@gmail.com>
2025-03-20 21:01:10 -0700

    ref: Removes debugging logs from /backend/authenticateToken.js

--- backend/authenticateToken.js
+++ backend/authenticateToken.js
@@ -3,7 +3,6 @@ import admin from 'firebase-admin';
 
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     console.log('Unauthorized: Token missing or expired');
     return res.status(401).json({message: 'Unauthorized: Token missing or expired'});
@@ -13,7 +12,7 @@ const authenticateToken = async (req, res, next) => {
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     req.user = decodedToken;
-    console.log('User authenticated:', decodedToken);
+
     next();
   } catch (error) {
     console.error('Token verification failed:', error);

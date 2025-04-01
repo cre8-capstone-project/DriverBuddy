50214b2cf6ca4de896cbba0e0561d45c7c9f6cd8
terustestacount <terumasamori@gmail.com>
2025-03-31 21:45:55 -0700

    fix: add gradient on "Turn on detection" button #CRE8-269

--- frontend/components/TurnOnDetectionButton.tsx
+++ frontend/components/TurnOnDetectionButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
 import {Icon, useTheme} from '@rneui/themed';
+import {LinearGradient} from 'expo-linear-gradient';
 
 interface TurnOnDetectionButtonProps {
   onPress?: () => void;
@@ -11,21 +12,22 @@ const TurnOnDetectionButton: React.FC<TurnOnDetectionButtonProps> = ({onPress, d
   const {theme} = useTheme();
 
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      disabled={disabled}
-      style={[
-        styles.button,
-        {
-          backgroundColor: disabled ? theme.colors.grey3 : theme.colors.primary,
-          borderWidth: 0,
-        },
-        styles.container,
-      ]}>
-      <Icon name="videocam" type="material" color="white" size={40} style={styles.icon} />
-      <View>
-        <Text style={styles.text}>Turn On{'\n'}detection</Text>
-      </View>
+    <TouchableOpacity onPress={onPress} disabled={disabled} style={styles.container}>
+      {disabled ? (
+        <View style={[styles.button, {backgroundColor: theme.colors.grey3}]}>
+          <Icon name="videocam" type="material" color="white" size={40} style={styles.icon} />
+          <Text style={styles.text}>Turn On{'\n'}detection</Text>
+        </View>
+      ) : (
+        <LinearGradient
+          colors={['rgba(0, 255, 255, 1)', 'rgba(30, 58, 138, 1)']}
+          start={{x: 0, y: 2}}
+          end={{x: 1, y: -1}}
+          style={styles.button}>
+          <Icon name="videocam" type="material" color="white" size={40} style={styles.icon} />
+          <Text style={styles.text}>Turn On{'\n'}detection</Text>
+        </LinearGradient>
+      )}
     </TouchableOpacity>
   );
 };
@@ -35,20 +37,18 @@ const styles = StyleSheet.create({
     marginLeft: 5,
   },
   button: {
-    flex: 1,
     width: 180,
     height: 64,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 44,
-    // borderWidth: 2,
     paddingHorizontal: 11,
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 4},
     shadowOpacity: 0.1,
     shadowRadius: 8,
-    elevation: 3, // Android の影
+    // elevation: 3,
   },
   icon: {
     marginRight: 8,
